a14378350b6d238252b6648c53b64c02a4d303bc
Michael Billington <michael.billington@gmail.com>
2013-04-04 21:20:17 +1100

    fixes for internal link issues #13

--- api/util/parser.php
+++ api/util/parser.php
@@ -8,31 +8,47 @@ require_once(dirname(__FILE__) . "/../../vendor/wikitext/wikitext.php");
 class SmParserBackend extends DefaultParserBackend {
 
 	public function getInternalLinkInfo($info) {
-		if($info['namespace'] == 'word') {
-			/* Link to a word and display it with subscript number */
-			$newinfo = $info;
-			$newinfo['url'] = core::constructURL('word', 'view', array($info['target']), 'html');
-			$wlpart = word_model::getSpellingAndNumberFromStr($info['target']);
-
-			if($info['title'] == $info['caption']) {
-				/* Change caption if user hasn't set their own */
-				$newinfo['caption'] = $wlpart['spelling'];
-				if($wlpart['number'] != 0) {
-					$newinfo['caption'] .= "<sub><small>".(int)$wlpart['number']."</small></sub>";
+		switch($info['namespace']) {
+			case 'word':
+				/* Link to a word and display it with subscript number */
+				$info['url'] = core::constructURL('word', 'view', array($info['target']), 'html');
+				if(trim($info['target']) == '') {
+					/* Allow linking to [[word:]] for the vocab */
+					break;
 				}
-			}
 
-			/* Figure out ID string and look it up */
-			$idstr = word_model::getIdStrBySpellingNum($wlpart['spelling'], $wlpart['number']);
-			if(word_model::getWordIDfromStr($idstr) === false) {
-				$newinfo['exists'] = false;
-				$newinfo['title'] = $idstr . " (definition not known)";
-			} else {
-				$newinfo['title'] = $idstr;
-			}
-			return $newinfo;
-		} else {
-			$info['url'] = core::constructURL('page', 'view', array($info['target']), 'html');
+				$newinfo = $info;
+				$wlpart = word_model::getSpellingAndNumberFromStr($info['target']);
+
+				if($info['title'] == $info['caption']) {
+					/* Change caption if user hasn't set their own */
+					$newinfo['caption'] = $wlpart['spelling'];
+					if($wlpart['number'] != 0) {
+						$newinfo['caption'] .= "<sub><small>".(int)$wlpart['number']."</small></sub>";
+					}
+				}
+
+				/* Figure out ID string and look it up */
+				$idstr = word_model::getIdStrBySpellingNum($wlpart['spelling'], $wlpart['number']);
+				if(word_model::getWordIDfromStr($idstr) === false) {
+					$newinfo['exists'] = false;
+					$newinfo['title'] = $idstr . " (definition not known)";
+				} else {
+					$newinfo['title'] = $idstr;
+				}
+				return $newinfo;
+			case 'ex':
+				$info['url'] = core::constructURL('example', 'view', array($info['target']), 'html');
+				break;
+			default:
+				if($info['namespace'] == "") {
+					/* Only look at non-namespaced links to avoid breaking interwiki links */
+					if(!$page = page_model::getByShort($info['target'])) {
+						/* Red-link pages which don't exist */
+						$info['exists'] = false;
+					}
+					$info['url'] = core::constructURL('page', 'view', array($info['title']), 'html');
+				}
 		}
 		return $info;
 	}
