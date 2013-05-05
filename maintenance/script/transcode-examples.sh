34d897fa6006c3f1edd9b01dfbffee61b6baf301
Michael Billington <michael.billington@gmail.com>
2013-05-05 13:55:06 +1000

    add audio transcoding script (two formats improves cross-browser support)

---
+++ maintenance/script/transcode-examples.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# This script converts new .ogg recordings to .mp3
+# Between these two formats, all normal browsers are supported.
+
+# Check LAME exists
+ffmpeg="ffmpeg"
+cmd=$(type -P $ffmpeg)
+if [ -z "$cmd" ]; then
+	echo "Install ffmpeg to use this script (command $ffmpeg not found) "
+	exit 1
+fi
+
+# Move to OGG directory for words
+cd ../../data/audio/spelling/ogg
+mkdir -p ../mp3/
+for name in *.ogg; do
+	# Modified form of http://linuxforums.org.uk/index.php?topic=9836.0
+	dest="../mp3/${name/.ogg/.mp3}"
+	if [ ! -f $dest ]; then
+		# Only transcode files that don't exist
+		ffmpeg -i "$name" -ab 128k -map_metadata 0:0,s0 "$dest";
+	fi
+done;
+
+# Repeat for examples
+cd ../../example/ogg
+mkdir -p ../mp3/
+for name in *.ogg; do
+	# Modified form of http://linuxforums.org.uk/index.php?topic=9836.0
+	dest="../mp3/${name/.ogg/.mp3}"
+	if [ ! -f $dest ]; then
+		# Only transcode files that don't exist
+		ffmpeg -i "$name" -ab 128k -map_metadata 0:0,s0 "$dest";
+	fi
+done;
