9ae4f452d48e06b59535ca70f213d9c360d27f5c
Michael Billington <michael.billington@gmail.com>
2014-01-31 21:38:06 +1100

    correction, split() is now deprecated

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $config = core::getConfig('core');
 
 /* Get page (or go to default if none is specified) */
 if(isset($_GET['p']) && $_GET['p'] != '') {
-	$arg = split('/', $_REQUEST['p']);
+	$arg = explode('/', $_REQUEST['p']);
 } else {
 	$arg = $config['default']['arg'];
 }
